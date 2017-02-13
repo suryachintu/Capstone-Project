50a323d450bbe497b2d6f9e3262f7c58678d4cf7
suryachintu <suryaparsa@gmail.com>
2017-02-14 00:13:06 +0530

    bug : Fetching data using OkHttp
    OkHttp could not fetch the full response. Response did not show the full data as the original data to be retrieved.

--- app/src/main/java/com/surya/quakealert/MainActivityFragment.java
+++ app/src/main/java/com/surya/quakealert/MainActivityFragment.java
@@ -5,23 +5,33 @@ import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.IOException;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
 
+    private static final String TAG = MainActivityFragment.class.getSimpleName();
     private Unbinder unbinder;
     @BindView(R.id.quake_recyclerView)
     RecyclerView mRecyclerView;
     private QuakeAdapter mQuakeAdapter;
+    private OkHttpClient mOkHttpClient;
 
     public MainActivityFragment() {
     }
@@ -41,7 +51,46 @@ public class MainActivityFragment extends Fragment {
 
         mRecyclerView.setAdapter(mQuakeAdapter);
 
+        mOkHttpClient = new OkHttpClient();
+
+
+        try {
+            String response = fetchData();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+//        Log.e(TAG,response);
+
         return rootView;
+    }
+
+    private String fetchData() throws IOException {
+
+        Request request = new Request.Builder()
+                                .url(Utility.USGS_URL)
+                                .build();
+
+        Response response = null;
+
+        mOkHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e(TAG,"On Failure OkHttp " + e.getMessage());
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+
+                if (response.isSuccessful()){
+
+                    Log.e(TAG,response.body().string() + "");
+                }
+            }
+        });
+
+        return null;
     }
 
     @Override
---
+++ app/src/main/java/com/surya/quakealert/Utility.java
@@ -0,0 +1,11 @@
+package com.surya.quakealert;
+
+/**
+ * Created by Surya on 13-02-2017.
+ */
+
+public class Utility {
+
+    public static final String USGS_URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_day.geojson";
+
+}
